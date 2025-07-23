b7f6cda9906f5509a54636768d78c6f56d84d3f5
Wiktor <wiktor.jurek@googlemail.com>
2025-07-23 22:06:23 +0200

    fix: add suppressHydrationWarning attribute to html tag in RootLayout for improved hydration handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <head>
         <Script
           defer
